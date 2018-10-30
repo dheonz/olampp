b3d193e5b7700401ecc98438aa3693538f663a20
Dheonz Kuzuma <dev3kusumah@gmail.com>
2018-10-31 02:29:52 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 clear
 apt-get install sudo -y
-sudo apt-get install curl -y
-sudo apt-get install perl -y
-sudo apt-get install toilet -y
-sudo apt-get install figlet -y
-sudo apt-get install lolcat -y
-sudo apt-get install git -y
-sudo apt-get install apache2 -y
+apt-get install curl -y
+apt-get install perl -y
+apt-get install toilet -y
+apt-get install figlet -y
+apt-get install lolcat -y
+apt-get install git -y
+apt-get install apache2 -y
 etc/init.d/apache2 restart
-sudo apt-get install mysql-server -y
+apt-get install mysql-server -y
 etc/init.d/mysql restart
-sudo apt-get install php5-common libapache2-mod-php5 php5-cli -y
-sudo apt-get install php5-mysql php5-curl -y
-sudo apt-get install phpmyadmin -y
+apt-get install php5-common libapache2-mod-php5 php5-cli -y
+apt-get install php5-mysql php5-curl -y
+apt-get install phpmyadmin -y
 wget -qO- https://raw.github.com/lmbbox/lampp/master/application/bin/install.sh | sh
 apt-get clean
 wget https://raw.githubusercontent.com/dheonz/script/master/ap && mv -f ap /bin/lamp && chmod 777 /bin/lamp
