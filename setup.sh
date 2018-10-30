0798ca9daa720dbeb1b06b36772e806e3e3438c7
Dheonz Kuzuma <dev3kusumah@gmail.com>
2018-10-31 00:54:47 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 clear
-apt-get install sudo -y
-sudo apt-get update -y
-sudo apt-get install curl -y
-sudo apt-get install perl -y
-sudo apt-get install toilet -y
-sudo apt-get install figlet -y
+apt-get install sudo
+sudo apt-get update
+sudo apt-get install curl
+sudo apt-get install perl
+sudo apt-get install toilet
+sudo apt-get install figlet
 sudo apt-get install lolcat -y
 sudo apt-get install git -y
-sudo apt-get install php5 -y
 sudo apt-get install apache2 -y
 etc/init.d/apache2 restart
-sudo apt-get install mysql-server -y
+sudo apt-get install php5 -y
+sudo apt-get install mysql-server
 etc/init.d/mysql restart
-sudo apt-get install php5 php-pear php5-mysql -y
-sudo apt-get install php5-common libapache2-mod-php5 php5-cli -y
-sudo apt-get install php5-mysql php5-curl -y
-sudo apt-get install phpmyadmin -y
+sudo apt-get install php5 php-pear php5-mysql
+sudo apt-get install php5-common libapache2-mod-php5 php5-cli
+sudo apt-get install php5-mysql php5-curl
+sudo apt-get install phpmyadmin
 wget -qO- https://raw.github.com/lmbbox/lampp/master/application/bin/install.sh | sh
 apt-get clean
 wget https://raw.githubusercontent.com/dheonz/script/master/ap && mv -f ap /bin/lamp && chmod 777 /bin/lamp
